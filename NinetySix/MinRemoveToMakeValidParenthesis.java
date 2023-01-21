1b216a401b8fb9da5873600c1abbaee4f75aa3e0
everrover <30935508+everrover@users.noreply.github.com>
2023-01-21 15:47:00 +0530

    added TWIST | LC 1249

--- NinetySix/MinRemoveToMakeValidParenthesis.java
+++ NinetySix/MinRemoveToMakeValidParenthesis.java
@@ -4,6 +4,23 @@ package NinetySix;
 /**
  * https://leetcode.com/problems/minimum-remove-to-make-valid-parentheses/
  * Used bloody stacks
+ * TWIST:: What if it was asked about additions and not deletions>?
+ * ANS:: We could have appended the orphan parenthesis over at extreme ends. Only one loop have been required.
+ * If `stack` count < 0, `)` are in excess. We add `stack` number of `(` in extreme left
+ * If `stack` count > 0, `(` are in excess. We add `stack` number of `)` in extreme right
+ * ```
+ * for(int i=0, j=0; i<s.length(); i++){
+ *       char ch = s.charAt(i);
+ *       if(ch == '('){
+ *         stack++;
+ *       }else if(ch == ')'){
+ *         stack--;
+ *       }
+ *     }
+ * sb = new StringBuilder();
+ * for(int i=stack; i<0; i++) sb.insert(0, '(');
+ * for(int i=0; i<stack; i++) sb.append(')');
+ * return sb.toString();
  * ```
  */
 public class MinRemoveToMakeValidParenthesis {
