db9dc942af7fa9405eee599edfa07d96e8ed1bab
Abhishek Deyol <30935508+everrover@users.noreply.github.com>
2023-09-30 22:39:43 +0530

    Create MinimumOperationsToCollectElements.java for LC2869

---
+++ LordShiva/MinimumOperationsToCollectElements.java
@@ -0,0 +1,22 @@
+class Solution {
+  /**
+  https://leetcode.com/problems/minimum-operations-to-collect-elements/
+  
+  Tags:: #easy #set
+  **/
+  public int minOperations(List<Integer> nums, int k) {
+    int steps = 0;
+    Set<Integer> s = new HashSet<>();
+    int i=nums.size()-1;
+    for(; i>=0; i--){
+      steps++;
+      if(nums.get(i) >= 1 && nums.get(i) <= k){
+        s.add(nums.get(i));
+      }
+      if(s.size() == k){
+        break;
+      }
+    }
+    return steps;
+  }
+}
